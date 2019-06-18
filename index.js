6874d4f554a25fbefc6db912b51cff48576d8488
Manal HW <mnleen@gmail.com>
2019-06-18 11:22:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name ='Janet';
+var Name ='Janet';
 var height = 74;
 
 console.log("Name:");
-console.log(name);
+console.log(Name);
 
 console.log("Height:");
 console.log(height);
